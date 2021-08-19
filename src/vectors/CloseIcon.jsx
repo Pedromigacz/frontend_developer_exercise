074049123076db7e9e14f2025b1fbbe9a75e7c8b
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-19 14:50:39 -0300

    add closeIcon vector

---
+++ src/vectors/CloseIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CloseIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    fill="none"
+    viewBox="0 0 48 48"
+  >
+    <circle cx="24" cy="24" r="24" fill="#C4C4C4"></circle>
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M15.723 14.562l-1.768 1.767 7.637 7.637-7.955 7.955 1.697 1.697 7.955-7.955 7.637 7.637 1.767-1.768-7.636-7.637 7.954-7.955-1.697-1.697-7.954 7.955-7.637-7.636z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export default CloseIcon;
