b0809ffd5c17cb64a64cf64f8df5a57fab3409ae
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-19 14:51:18 -0300

    populate main with mockUp postits

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "../styles/Main.module.css";
+import { PostIt } from "./";
 
 const Main = () => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{"Notes (1)"}</h1>
+      <ul className={styles.postItContainer}>
+        <PostIt />
+        <PostIt />
+      </ul>
     </main>
   );
 };
