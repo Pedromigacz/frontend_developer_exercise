96aecfadadcfdca26a4cc7f67c1fbcb56ee2ab63
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-20 00:24:02 -0300

    refactor notesDisplayer to consume notesContext

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NotesContext } from "../contexts/NotesContext.jsx";
 import styles from "../styles/Main.module.css";
 import { PostIt } from "./";
 
 const Main = () => {
+  const { countNotes, notes, removeNote } = useContext(NotesContext);
+
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>{"Notes (1)"}</h1>
+      <h1 className={styles.title}>{`Notes (${countNotes()})`}</h1>
       <ul className={styles.postItContainer}>
-        <PostIt />
-        <PostIt />
+        {notes.length &&
+          notes.map((note, i) => (
+            <PostIt
+              title={note.title}
+              description={note.description}
+              close={() => {
+                removeNote(i);
+              }}
+              key={i}
+            />
+          ))}
       </ul>
     </main>
   );
