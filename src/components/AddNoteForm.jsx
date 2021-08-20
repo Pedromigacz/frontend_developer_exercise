6cc4b770080fd905af79068da57408dd8ac9665f
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-19 23:11:05 -0300

    fill AddNoteForm

--- src/components/AddNoteForm.jsx
+++ src/components/AddNoteForm.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "../styles/AddNoteForm.module.css";
+import { Input, Button } from "./";
 
 const AddNoteForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <aside className={styles.addNoteForm}>
-      <h2>Add a note</h2>
+      <form onSubmit={handleSubmit}>
+        <h2>Add a note</h2>
+        <Input label="Title:" placeholder="Enter a note title..." />
+        <Input
+          label="Description:"
+          placeholder="Enter a description..."
+          textArea="true"
+        />
+        <Button>Add note</Button>
+      </form>
     </aside>
   );
 };
