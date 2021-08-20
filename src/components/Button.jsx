695e8fc71c7b61547bbfad9db09ea6adf0eed35a
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-20 03:16:32 -0300

    Add AddNote button to Main component when mobile width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "../styles/Button.module.css";
 
-const Button = ({ children }) => {
+const Button = ({ children, onClick }) => {
   return (
     <div className={styles.buttonContainer}>
-      <button className={styles.button}>{children}</button>
+      <button className={styles.button} onClick={onClick}>
+        {children}
+      </button>
     </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from "react";
 import { NotesContext } from "../contexts/NotesContext.jsx";
+import { MobileContext } from "../contexts/MobileContext.jsx";
 import styles from "../styles/Main.module.css";
-import { PostIt } from "./";
+import { PostIt, Button } from "./";
 
 const Main = () => {
   const { countNotes, notes, removeNote } = useContext(NotesContext);
+  // mobile context
+  const { openAside } = useContext(MobileContext);
 
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>{`Notes (${countNotes()})`}</h1>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>{`Notes (${countNotes()})`}</h1>
+        <span className={styles.mobileButtonContainer}>
+          <Button onClick={openAside}>Add note</Button>
+        </span>
+      </div>
       <ul className={styles.postItContainer}>
         {notes.length &&
           notes.map((note, i) => (
