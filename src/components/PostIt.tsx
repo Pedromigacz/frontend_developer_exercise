082ef2ab0fa8dbf6a2099632047e8f29030781ba
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-20 18:39:17 -0300

    refactor PostIt component to TSX standart

--- src/components/PostIt.tsx
+++ src/components/PostIt.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "../styles/PostIt.module.css";
 import CloseIcon from "../vectors/CloseIcon.jsx";
 
-const PostIt = ({
-  title = "Sticky note title here",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
-  close,
-}) => {
+type PostItProps = {
+  title: string;
+  description: string;
+  close: () => void;
+};
+
+const PostIt = ({ title, description, close }: PostItProps) => {
   return (
     <li className={styles.postIt}>
       <button className={styles.closeButton} onClick={close}>
