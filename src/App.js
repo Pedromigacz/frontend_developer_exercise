092d8921bcdcbef38eb783c3f6877410033b4cda
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-20 13:44:40 -0300

    animate AddNoteForm on mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Layout, Main, AddNoteForm } from "./components/";
 import NotesContextProvider from "./contexts/NotesContext.jsx";
 import MobileContextProvider from "./contexts/MobileContext.jsx";
+// import { AnimatePresence } from "framer-motion";
 
 const App = () => (
   <MobileContextProvider>
     <NotesContextProvider>
       <Layout>
         <Main />
+        {/* <AnimatePresence> */}
         <AddNoteForm />
+        {/* </AnimatePresence> */}
       </Layout>
     </NotesContextProvider>
   </MobileContextProvider>
--- src/components/AddNoteForm.jsx
+++ src/components/AddNoteForm.jsx
@@ -4,6 +4,7 @@ import { MobileContext } from "../contexts/MobileContext.jsx";
 import styles from "../styles/AddNoteForm.module.css";
 import { Input, Button } from "./";
 import CloseIcon from "../vectors/CloseIcon.jsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 const AddNoteForm = () => {
   // form state
@@ -26,34 +27,44 @@ const AddNoteForm = () => {
     closeAside();
   };
 
-  return !mobile || displayAside ? (
-    <aside className={styles.addNoteForm}>
-      <button className={styles.closeButton} onClick={closeAside}>
-        <CloseIcon />
-      </button>
-      <form onSubmit={handleSubmit}>
-        <h2>Add a note</h2>
-        <Input
-          label="Title:"
-          placeholder="Enter a note title..."
-          value={newNote.title}
-          onChange={(e) => {
-            setNewNote({ ...newNote, title: e.target.value });
-          }}
-        />
-        <Input
-          label="Description:"
-          placeholder="Enter a description..."
-          textArea="true"
-          value={newNote.description}
-          onChange={(e) => {
-            setNewNote({ ...newNote, description: e.target.value });
-          }}
-        />
-        <Button>Add note</Button>
-      </form>
-    </aside>
-  ) : null;
+  return (
+    <AnimatePresence>
+      {!mobile || displayAside ? (
+        <motion.aside
+          className={styles.addNoteForm}
+          initial={{ x: 600, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: 600, opacity: 0 }}
+          transition={{ duration: 0.4, type: "tween" }}
+        >
+          <button className={styles.closeButton} onClick={closeAside}>
+            <CloseIcon />
+          </button>
+          <form onSubmit={handleSubmit}>
+            <h2>Add a note</h2>
+            <Input
+              label="Title:"
+              placeholder="Enter a note title..."
+              value={newNote.title}
+              onChange={(e) => {
+                setNewNote({ ...newNote, title: e.target.value });
+              }}
+            />
+            <Input
+              label="Description:"
+              placeholder="Enter a description..."
+              textArea="true"
+              value={newNote.description}
+              onChange={(e) => {
+                setNewNote({ ...newNote, description: e.target.value });
+              }}
+            />
+            <Button>Add note</Button>
+          </form>
+        </motion.aside>
+      ) : null}
+    </AnimatePresence>
+  );
 };
 
 export default AddNoteForm;
