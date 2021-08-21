abcc616c91b6e5829c22f7163b3495ef002e8de5
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-08-21 01:18:59 -0300

    refactor Layout component to TSX standart

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../styles/Layout.module.css";
 
-const Layout = ({ children }) => {
+const Layout: React.FC = ({ children }) => {
   return <div className={styles.layoutContainer}>{children}</div>;
 };
 
